04377494386ec57fda0169762d8711b42dc0e2d0
modem7 <modem7@gmail.com>
2022-05-06 02:17:55 +0100

    Update create-cloud-template.sh
    Now uses non-KVM image.
    
    This helps out with the serial console issue
    
    Added EFI drive
    
    Changed from SeaBIOS to OVMF (EFI BIOS)

--- Bash/Proxmox Scripts/create-cloud-template.sh
+++ Bash/Proxmox Scripts/create-cloud-template.sh
@@ -21,8 +21,8 @@ if [ "" = "$PKG_OK" ]; then
 fi
 
 # Image variables
-SRC_IMG="https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64-disk-kvm.img"
-IMG_NAME="focal-server-cloudimg-amd64-disk-kvm.qcow2"
+SRC_IMG="https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
+IMG_NAME="focal-server-cloudimg-amd64.qcow2"
 WORK_DIR="/tmp"
 
 # Download image
@@ -31,7 +31,7 @@ wget -O $WORK_DIR/$IMG_NAME $SRC_IMG
 
 # Image variables
 OSNAME="Ubuntu 20.04"
-TEMPL_NAME="ubuntu-cloud-master"
+TEMPL_NAME="ubuntu2004-cloud-master"
 VMID_DEFAULT="52000"
 read -p "Enter a VM ID for $OSNAME [$VMID_DEFAULT]: " VMID
 VMID=${VMID:-$VMID_DEFAULT}
@@ -43,6 +43,7 @@ CLOUD_PASSWORD_DEFAULT=$GENPASS
 read -p "Enter a Cloud-Init Password for $OSNAME [$CLOUD_PASSWORD_DEFAULT]: " CLOUD_PASSWORD
 CLOUD_PASSWORD=${CLOUD_PASSWORD:-$CLOUD_PASSWORD_DEFAULT}
 MEM="2048"
+BALLOON="512"
 DISK_SIZE="15G"
 DISK_STOR="Proxmox"
 NET_BRIDGE="vmbr1"
@@ -53,23 +54,24 @@ OS_TYPE="l26"
 AGENT_ENABLE="1" #change to 0 if you don't want the guest agent
 FSTRIM="1"
 CITYPE="nocloud"
+BIOS="ovmf" # Choose between ovmf or seabios
 
 # install qemu-guest-agent inside image
 virt-customize --install qemu-guest-agent -a $IMG_NAME
 
 # create VM
-qm create $VMID --name $TEMPL_NAME --memory $MEM --cores $CORES --net0 virtio,bridge=${NET_BRIDGE}${VLAN:+,tag=$VLAN}
+qm create $VMID --name $TEMPL_NAME --memory $MEM --balloon $BALLOON --cores $CORES --bios $BIOS --net0 virtio,bridge=${NET_BRIDGE}${VLAN:+,tag=$VLAN}
 qm set $VMID --agent enabled=$AGENT_ENABLE,fstrim_cloned_disks=$FSTRIM
 qm set $VMID --ostype $OS_TYPE
 qm importdisk $VMID $WORK_DIR/$IMG_NAME $DISK_STOR -format qcow2
 qm set $VMID --scsihw virtio-scsi-pci --scsi0 $DISK_STOR:$VMID/vm-$VMID-disk-0.qcow2,cache=writethrough,discard=on
-qm set $VMID --ide2 $DISK_STOR:cloudinit
+qm set $VMID --scsi1 $DISK_STOR:cloudinit
+qm set $VMID --efidisk0 $DISK_STOR:0,efitype=4m,,format=qcow2,pre-enrolled-keys=1,size=528K
 qm set $VMID --citype $CITYPE
 qm set $VMID --ciuser $CLOUD_USER
 qm set $VMID --cipassword $CLOUD_PASSWORD
 qm set $VMID --boot c --bootdisk scsi0
 qm set $VMID --ipconfig0 ip=dhcp
-qm set $VMID --serial0 socket --vga serial0
 qm resize $VMID scsi0 $DISK_SIZE
 
 # Delete previously downloaded file
