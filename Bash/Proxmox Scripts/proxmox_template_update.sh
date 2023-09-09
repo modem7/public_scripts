f376e17bc3d12fec30d492dc0c3f331f03c3b50c
modem7 <modem7@gmail.com>
2023-09-09 04:03:38 +0100

    updated script

--- Bash/Proxmox Scripts/proxmox_template_update.sh
+++ Bash/Proxmox Scripts/proxmox_template_update.sh
@@ -1,3 +1,4 @@
+# Step 1
 # Destroy current templates
 for VM in 600{0..2}
 do
@@ -5,12 +6,14 @@ qm destroy $VM --destroy-unreferenced-disks 1 --purge &
 done
 wait
 
+# Step 2
 # Clone from master VM's
 qm clone 9001 6000 --name ubuntu-desktop-cloud-master-template & \
 qm clone 9002 6001 --name ubuntu2204-cloud-master-template & \
 qm clone 9003 6002 --name ubuntu2204-cloud-master-extras-template & \
 wait
 
+# Step 3
 # Set Cloud-Init to DHCP
 for VM in 600{0..2}
 do
@@ -18,6 +21,7 @@ qm set $VM --ipconfig0 ip=dhcp &
 done
 wait
 
+# Step 4
 # Start VM's
 for VM in 600{0..2}
 do
@@ -25,6 +29,7 @@ qm start $VM &
 done
 wait
 
+# Step 5
 # Run cleanup on templates
 for VM in 600{0..2}
 do
@@ -41,6 +46,7 @@ qm guest exec $VM -- /bin/bash -c "history -w"
 qm guest exec $VM -- /bin/bash -c "fstrim -av"
 done
 
+# Step 6
 # Shutdown templates
 for VM in 600{0..2}
 do
@@ -48,6 +54,7 @@ qm shutdown $VM &
 done
 wait
 
+# Step 7
 # Convert to template
 for VM in 600{0..2}
 do
